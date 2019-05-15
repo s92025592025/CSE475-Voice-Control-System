5588def7c6c1c35327e2b938ebcebc923243f97e
Daniel Wang <s92025592025@gmail.com>
2019-05-14 21:42:43 -0700

    can seemed to verify credental and refresh

--- NLP_Control.py
+++ NLP_Control.py
@@ -5,6 +5,7 @@ import io
 import json
 import click
 import google.oauth2.credentials
+import google.auth.transport.requests
 
 CREDENTIAL_FILE = click.get_app_dir('google-oauthlib-tool') + "/credentials.json"
 
@@ -33,7 +34,7 @@ def gAssisantAuth():
 			http_request = google.auth.transport.requests.Request()
 
 			# Refresh the credential, don't know why but won't hurt
-			credential.refresh()
+			credential.refresh(http_request)
 			print("Credential Verified")
 	except Exception as e:
 		print("Something is wrong with the credential. ", e)
