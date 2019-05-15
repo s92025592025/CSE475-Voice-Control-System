3a6c5fceea61274731611e0765364517553423ca
Daniel Wang <s92025592025@gmail.com>
2019-05-15 10:22:36 -0700

    can create api service

--- GoogleAssistant.py
+++ GoogleAssistant.py
@@ -5,6 +5,7 @@ import json
 import google.oauth2.credentials
 import google.auth.transport.requests
 import google.auth.transport.grpc
+from google.assistant.embedded.v1alpha2 import embedded_assistant_pb2_grpc 
 
 try:
 	from googlesamples.assistant.grpc import (
@@ -21,10 +22,13 @@ communication process with Google Assistant SDK(Service)
 """
 class GoogleAssistant:
 	ASSISTANT_API_ENDPOINT = 'embeddedassistant.googleapis.com'
+	DEFAULT_GRPC_DEADLINE = 60 * 3 + 5
 
 	def __init__(self):
 		self.__CREDENTIAL_FILE = click.get_app_dir('google-oauthlib-tool') + "/credentials.json"
 		self.__authentication()
+		self.__audioSetup()
+		self.__create_assistant()
 
 	
 	"""
@@ -47,17 +51,22 @@ class GoogleAssistant:
 			print("Something is wrong with the credential. ", e)
 			sys.exit(-1)
 
-		self.channel = google.auth.transport.grpc.secure_authorized_channel(
-												self.credential,
-												self.http_request,
-												GoogleAssistant.ASSISTANT_API_ENDPOINT)
 
-		#return self.channel
 
-	def audioSetup(self):
+	def __audioSetup(self):
 		self.audio_sample_rate = audio_helpers.DEFAULT_AUDIO_SAMPLE_RATE
 		self.audio_sample_width = audio_helpers.DEFAULT_AUDIO_SAMPLE_WIDTH
 		self.audio_iter_size = audio_helpers.DEFAULT_AUDIO_ITER_SIZE
 		self.audio_block_size = audio_helpers.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE
 		self.audio_flush_size = audio_helpers.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE
-		self.grpc_deadline = DEFAULT_GRPC_DEADLINE
+		self.grpc_deadline = GoogleAssistant.DEFAULT_GRPC_DEADLINE
+		self.conversation_state_bytes = None
+		self.volume_percentage = 50
+
+	def __create_assistant(self):
+		self.channel = google.auth.transport.grpc.secure_authorized_channel(
+												self.credential,
+												self.http_request,
+												GoogleAssistant.ASSISTANT_API_ENDPOINT)
+		print("GRPC Channel Created")
+		self.assistant = embedded_assistant_pb2_grpc.EmbeddedAssistantStub(self.channel)
