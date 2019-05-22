411ed03e983194e3f027a020db0d6e6a9b1a5255
Daniel Wang <s92025592025@gmail.com>
2019-05-21 17:17:16 -0700

    can search for song

--- youtubePlayer.py
+++ youtubePlayer.py
@@ -1,6 +1,7 @@
 import sys
 import click
 import json
+import io
 import pafy
 
 import googleapiclient.discovery
@@ -12,14 +13,28 @@ class YoutubePlayer:
 	def __init__(self):
 		# Getting our api key
 		apiKeyFile = click.get_app_dir("youtube-api-key") + "/key.json"
-		apiKeyJson = json.load(apiKeyFile)
-		self.__API_KEY = apiKeyJson['api_key']
+		self.__API_KEY = self.__getAPIKey(apiKeyFile)
 
 		# Setup pafy
 		pafy.set_api_key(self.__API_KEY)
 
 		# Setup youtube api
 		self.__youtubeApi = self.__youtubeApiFactory(self.__API_KEY)
+
+	"""
+	Gets the api key from pointed location
+	@param path - The path to the json file containing server API key
+	"""
+	def __getAPIKey(self, path):
+		try:
+			with io.open(path, 'r') as f:
+				keyJson = json.load(f)
+				output = keyJson['api_key']
+		except Exception as e:
+			print("Having issue reading ", path, e)
+			sys.exit(-1)
+
+		return output
 
 	"""
 	Generates a youtube api object
@@ -35,3 +50,23 @@ class YoutubePlayer:
 												 developerKey=apiKey)
 
 		return output
+
+	"""
+	Search for the music on youtube with the provided music name
+	@param musicName - The music name given by the user
+	@return a video id for the first search result, return None when on result us returned
+	"""
+	def searchSong(self, musicName):
+		print("Not implemented")
+		request = self.__youtubeApi.search().list(
+				part="snippet",
+				maxResults=1,
+				q=musicName
+		)
+
+		response = request.execute() # response is already a json object
+
+		if response['pageInfo']['totalResults'] <= 0:
+			return None
+
+		return response['items'][0]['id']['videoId']
