8d5dc12ff2f6f339a97de9eefaa18a8c09e112b5
Daniel Wang <s92025592025@gmail.com>
2019-05-27 15:12:38 -0700

    can make twitTER API OBJ

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -1,5 +1,33 @@
+import click
+import io
+import json
+import sys
+
 import twitter
 
 class TwitterAPI:
 	def __init__(self):
-		print("Not implemented")
+		apiKeyFilePath = click.get_app_dir("twitter-api") + "/credential.json"
+		keys = self.__getAPIKeys(apiKeyFilePath)
+		self.__API = twitter.Api(consumer_key = keys["consumer-api-key"],
+								 consumer_secret = keys["consumer-secret-key"],
+								 access_token_key = keys["access-token"],
+								 access_token_secret = keys["access-secret"]
+		)
+
+	"""
+	Get the twitter api information from the given path
+	@param path - The path to the credential.json file that contains the
+				  Twitter api information
+	@return A json object representing the content of the path
+	"""
+	def __getAPIKeys(self, path):
+		output = None
+		try:
+			with io.open(path, 'r') as f:
+				output = json.load(f)
+		except Exception as e:
+			print("Having issue reading ", path, e)
+			sys.exit(-1)
+
+		return output
