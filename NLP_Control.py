97f42bfbf3c601a064f045c28de25dc4eb5f81ef
Daniel Wang <s92025592025@gmail.com>
2019-05-28 16:20:26 -0700

    have a centralized entry

--- NLP_Control.py
+++ NLP_Control.py
@@ -17,35 +17,38 @@ def interrupt_callback():
     global interrupted
     return interrupted
 
-def detectedCallback():
-	detector.terminate() # So google Assistant can use audio device
-	snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING);
-	assistant.startAssist()
-	snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)
-	detector.start(detected_callback=detectedCallback,
-					interrupt_check=interrupt_callback,
-		            sleep_time=0.03)
 
+"""
 if len(sys.argv) == 1:
     print("Error: need to specify model name")
     print("Usage: python demo.py your.model")
     sys.exit(-1)
+"""
 
-model = sys.argv[1]
+def startNLP(model):
+	#model = sys.argv[1]
+	def detectedCallback():
+		detector.terminate() # So google Assistant can use audio device
+		snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING);
+		assistant.startAssist()
+		snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)
+		detector.start(detected_callback=detectedCallback,
+					   interrupt_check=interrupt_callback,
+		               sleep_time=0.03)
 
-i2c = I2C(SLAVE_ADDR)
-assistant = GoogleAssistant(i2c)
+	i2c = I2C(SLAVE_ADDR)
+	assistant = GoogleAssistant(i2c)
 
-# capture SIGINT signal, e.g., Ctrl+C
-signal.signal(signal.SIGINT, signal_handler)
+	# capture SIGINT signal, e.g., Ctrl+C
+	signal.signal(signal.SIGINT, signal_handler)
 
-# The obj contains the hotword detection
-detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
-print('Listening... Press Ctrl+C to exit')
+	# The obj contains the hotword detection
+	detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+	print('Listening... Press Ctrl+C to exit')
 
-# main loop
-detector.start(detected_callback=detectedCallback,#snowboydecoder.play_audio_file,
-               interrupt_check=interrupt_callback,
-               sleep_time=0.03)
+	# main loop
+	detector.start(detected_callback=detectedCallback,#snowboydecoder.play_audio_file,
+               	   interrupt_check=interrupt_callback,
+               	   sleep_time=0.03)
 
-detector.terminate()
+	detector.terminate()
