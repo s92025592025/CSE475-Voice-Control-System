30389e728e85801dfb9ac670be64db8a901f758e
Daniel Wang <s92025592025@gmail.com>
2019-06-04 20:33:37 -0700

    can connect, send receive and reconnect when disconnected

--- BT.py
+++ BT.py
@@ -1,3 +1,94 @@
+import sys
+import bluetooth
+import threading
+import concurrent.futures
+
 class Bluetooth:
-	def __init__():
-		print("just started")
+	RECEIVE_BUFFER = 1024
+	def __init__(self):
+		self.__PORT_NUM = 1
+		self.__KILL_LOCK = threading.Lock()
+		self.__toKill = 0
+		self.__createSoc()
+
+	"""
+	Creates socket
+	"""
+	def __createSoc(self):		
+		self.__BT_SOCKET = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+		self.__BT_SOCKET.bind(("", self.__PORT_NUM))
+		self.__BT_SOCKET.listen(1)
+
+	"""
+	"""
+	def acceptCon(self):
+		self.__CLIENT_SOC, self.__BT_ADDR = self.__BT_SOCKET.accept()
+		print("Accepted connection from ", self.__BT_ADDR)
+
+	"""
+	Receives data from connection
+	"""
+	def receive(self):
+		return str(self.__CLIENT_SOC.recv(Bluetooth.RECEIVE_BUFFER))
+
+	"""
+	Sends data out to client
+	"""
+	def send(self, out):
+		self.__CLIENT_SOC.send(out.encode())
+
+	"""
+	Set the variable saying whether the child theeads should be killed
+	@param toKill - 1 for the child to kill, 0 for not to kill
+	"""
+	def __setKillLock(self, toKill):
+		self.__KILL_LOCK.acquire()
+		print("in set kill")
+		self.__toKill = toKill
+		self.__KILL_LOCK.release()
+		print("out of setkill")
+
+	def __receiveEvent(self):
+		try:
+			while True:
+				received = self.receive()
+				print("Received: ", received)
+
+				# Send something to i2c
+		except Exception as e:
+			print("Excpetion in receive event: ", e)
+			self.__CLIENT_SOC.close()
+			self.__BT_SOCKET.close()
+			self.__setKillLock(1)
+			sys.exit(0)
+
+	def __sendEvent(self):
+		try:
+			while True:
+				# Get something from i2c
+
+				self.send("sensor1 8903\0")
+		except Exception as e:
+			print("Excpetion in send event: ", e)
+			self.__CLIENT_SOC.close()
+			self.__BT_SOCKET.close()
+			self.__setKillLock(1)
+			sys.exit(0)
+
+	"""
+	The bluetooth operation
+	"""
+	def operation(self):
+		self.acceptCon()
+		self.__toKill = 0	
+
+		e = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+		receiveE = e.submit(self.__receiveEvent)
+		sendE = e.submit(self.__sendEvent)
+		
+		while self.__toKill == 0:
+			pass # Hold if everything is fine
+		
+		print("try reconnect")
+		self.__createSoc()
+		self.operation()
